02465c9c14d9767ac5cd438bc16946070f49ffa6
Ana Janovic <janovicana97@gmail.com>
2021-07-02 15:22:59 +0200

    prvo deo

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer id="footer" class="footer">
+  <div class="container text-center">
+
+  <h3>Pratite nas na drustvenim mrezama</h3>
+
+  <ul class="social-links">
+    <li><a href="#link"><i class="fa fa-twitter fa-fw"></i></a></li>
+    <li><a href="#link"><i class="fa fa-facebook fa-fw"></i></a></li>
+    <li><a href="#link"><i class="fa fa-google-plus fa-fw"></i></a></li>
+    <li><a href="#link"><i class="fa fa-instagram fa-fw"></i></a></li>
+    <li><a href="#link"><i class="fa fa-linkedin fa-fw"></i></a></li>
+  </ul>
+
+   Muška odbojkaška superiliga Sribije
+  </div>
+</footer>
